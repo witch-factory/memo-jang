1b04eeb4cb388eb65ff7462bfba6a85ebcb12bc6
witch-factory <soakdma37@gmail.com>
2021-08-30 17:58:13 +0900

    making basic layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-// eslint-disable-next-line import/extensions
 import { Route } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 import Note from './note';
+
+const NoteGlobalStyle = createGlobalStyle`
+  body{
+    background:#e3fafc
+  }
+`;
 
 function App() {
   return (
     <div>
+      <NoteGlobalStyle />
       <Route path="/" component={Note} />
     </div>
   );
--- client/src/note.js
+++ client/src/note.js
@@ -1,16 +1,43 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 
-const NoteGlobalStyle = createGlobalStyle`
-  body{
-    background:#e3fafc
-  }
+const FlexContainer = styled.div`
+  display:flex;
+  margin:5px;
+`;
+
+const NoteBasicBlock = styled.div`
+  background: ${(props) => props.color || 'white'};
+  color:white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  font-size:1rem;
+  margin:3px;
+`;
+
+const NoteListButton = styled(NoteBasicBlock)`
+  width:8rem;
+  height:2.5rem;
+`;
+
+const NoteListBlock = styled(NoteBasicBlock)`
+  width:18rem;
+  height:2.5rem;
+  border:solid 1px #868e96;
+  background:#f1f3f5;
 `;
 
 const Note = () => (
   <>
-    <NoteGlobalStyle />
-    <h1>안녕하세요</h1>
+    <FlexContainer>
+      <NoteListButton color="#b197fc">노트 추가</NoteListButton>
+      <NoteListButton color="#ff6b6b">노트 삭제</NoteListButton>
+    </FlexContainer>
+    <NoteListBlock />
+    <NoteListBlock />
+    <NoteListBlock />
   </>
 );
 
