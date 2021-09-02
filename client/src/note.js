6d9298bf72e78c1a01b7e634733fe91a8adc32f2
witch-factory <soakdma37@gmail.com>
2021-09-02 19:46:42 +0900

    css 조정중, 버튼 hover

--- client/src/note.js
+++ client/src/note.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { darken } from 'polished';
 
 const FlexContainer = styled.div`
   display:flex;
-  margin:5px;
 `;
 
-const NoteContainer = styled(FlexContainer)`
+const ColumnContainer = styled(FlexContainer)`
+  flex-direction: column;
+`;
+
+const RowContainer = styled(FlexContainer)`
   height:100%;
   width:100%;
+  flex-direction: row;
 `;
 
 const NoteBasicBlock = styled.div`
@@ -19,12 +24,21 @@ const NoteBasicBlock = styled.div`
   align-items: center;
   border-radius: 10px;
   font-size:1rem;
-  margin:3px;
 `;
 
 const NoteListButton = styled(NoteBasicBlock)`
   width:8rem;
   height:2.5rem;
+  margin:5px;
+  ${(props) => {
+    const selected = props.color;
+    return css`
+      &:hover {
+        background: ${darken(0.1, selected)};
+      }
+    `;
+  }
+}
 `;
 
 const NoteListBlock = styled(NoteBasicBlock)`
@@ -32,17 +46,18 @@ const NoteListBlock = styled(NoteBasicBlock)`
   height:2.5rem;
   border:solid 1px #868e96;
   background:#f1f3f5;
+  margin:3px;
 `;
 
 const NoteEditBlock = styled.textarea`
   width:100%;
-  height:90%;
+  height:100%;
   border: 1px solid black;
   border-radius: 7px;
   overflow:auto;
   white-space: pre;
   font-size:12pt;
-  flex:1;
+  display:flex;
   background-attachment: local;
   background-image:
     linear-gradient(to right, white 10px, transparent 10px),
@@ -50,24 +65,25 @@ const NoteEditBlock = styled.textarea`
     repeating-linear-gradient(white, white 30px, #ccc 30px, #ccc 31px, white 31px);
   line-height: 31px;
   padding: 8px 10px;
+  margin:3px;
   resize:none;
 `;
 
 const Note = () => (
-  <>
-    <FlexContainer>
+  <ColumnContainer>
+    <RowContainer>
       <NoteListButton color="#b197fc">노트 추가</NoteListButton>
       <NoteListButton color="#ff6b6b">노트 삭제</NoteListButton>
-    </FlexContainer>
-    <NoteContainer>
-      <div>
+    </RowContainer>
+    <RowContainer>
+      <ColumnContainer>
         <NoteListBlock />
         <NoteListBlock />
         <NoteListBlock />
-      </div>
+      </ColumnContainer>
       <NoteEditBlock />
-    </NoteContainer>
-  </>
+    </RowContainer>
+  </ColumnContainer>
 );
 
 export default Note;
